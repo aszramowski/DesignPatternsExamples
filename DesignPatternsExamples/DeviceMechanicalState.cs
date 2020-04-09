5b22fb9b2069968b2cdaa5031b028804c4a0b4fe
aszramowski <artur.szramowski@apator.om>
2020-04-09 17:10:19 +0200

    Singleton - mechanical state of the fixture

---
+++ DesignPatternsExamples/DeviceMechanicalState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsExamples
+{
+    class DeviceMechanicalState
+    {
+        private static readonly object padLock = new object();
+        private static DeviceMechanicalState deviceMechanicalStateInstance;
+        public bool IsDeviceOpended { get; private set; }        
+        public static DeviceMechanicalState DeviceMechanicalStateInstance
+        {
+            get
+            {
+                lock (padLock)
+                {
+                    if (deviceMechanicalStateInstance == null)
+                        deviceMechanicalStateInstance = new DeviceMechanicalState();
+
+                    return deviceMechanicalStateInstance; 
+                }
+            }
+        }      
+        private DeviceMechanicalState() { }
+
+        public void CloseDevice()
+        {
+            if (IsDeviceOpended)
+                IsDeviceOpended = false;
+        }
+        public void OpenDevice()
+        {
+            if (!IsDeviceOpended)
+                IsDeviceOpended = true;
+        }
+    }
+}
--- DesignPatternsExamples/Program.cs
+++ DesignPatternsExamples/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace DesignPatternsExamples
 {
+    /// <summary>
+    /// This example shows a singleton that stores information about a mechanical state of an electrical device. The device can be opened or closed.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DeviceMechanicalState.DeviceMechanicalStateInstance.IsDeviceOpended);
+            DeviceMechanicalState.DeviceMechanicalStateInstance.OpenDevice();
+            Console.WriteLine(DeviceMechanicalState.DeviceMechanicalStateInstance.IsDeviceOpended);
+            DeviceMechanicalState.DeviceMechanicalStateInstance.OpenDevice();
+            Console.ReadKey();
         }
     }
 }
