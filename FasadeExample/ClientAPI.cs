2d408d04b2c3b0ba2c03380a4345f9f00097ccd2
aszramowski <artur.szramowski@apator.om>
2020-04-19 17:49:18 +0200

    -facade with bank account functionality

---
+++ FasadeExample/ClientAPI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class ClientAPI
+    {
+        private BallanceAccount ballanceAccount;
+        private MoneySpent moneySpent;
+        private MoneyTransfer moneyTransfer;
+
+        public ClientAPI()
+        {
+            this.ballanceAccount = new BallanceAccount();
+            this.moneySpent = new MoneySpent();
+            this.moneyTransfer = new MoneyTransfer();
+        }
+        public void Account()
+        {
+            ballanceAccount.DisplayStateAccount();
+        }
+        public void MoneySpend()
+        {
+            moneySpent.MoneySpentLastMonth();
+        }
+        public void MoneyTransfer()
+        {
+            moneyTransfer.MakeMoneyTransfer();
+        }
+    }
+    public class BallanceAccount
+    {
+        public void DisplayStateAccount()
+        {
+            Console.WriteLine("Wyświetlam stan konta : 10zł");
+        }
+    }
+    public class MoneySpent
+    {
+        public void MoneySpentLastMonth()
+        {
+            Console.WriteLine("Wyświetlam sumę wydaną w zeszłym miesiącu : 12zł");
+        }
+    }
+    public class MoneyTransfer
+    {
+        public void MakeMoneyTransfer()
+        {
+            Console.WriteLine("Wysłano pieniądze w kwocie : 3zł");
+        }
+    }
+}
---
+++ FasadeExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientAPI client = new ClientAPI();
+            client.Account();
+            client.MoneySpend();
+            client.MoneyTransfer();
+
+            Console.ReadKey();
+        }
+    }
+}
